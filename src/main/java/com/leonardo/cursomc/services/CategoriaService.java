e9e45d883ed23f02360c294031d2fd7f023b5359
Leonardo Silva da Cunha <leonardo.cunha@ntendencia.com.rb>
2020-01-13 10:46:56 -0300

    Estado e Cidade

--- src/main/java/com/leonardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/leonardo/cursomc/services/CategoriaService.java
@@ -1,11 +1,13 @@
 package com.leonardo.cursomc.services;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leonardo.cursomc.domain.Categoria;
 import java.util.Optional;
 import com.leonardo.cursomc.repositories.CategoriaRepository;
+
 
 @Service
 public class CategoriaService {
@@ -15,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
 	}
 	
 }
