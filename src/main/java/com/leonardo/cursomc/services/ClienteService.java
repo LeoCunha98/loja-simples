96334885351703a47104494a5d5e2a8e91ca1257
Leonardo Silva da Cunha <leonardo.cunha@ntendencia.com.rb>
2020-01-16 11:42:05 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/leonardo/cursomc/services/ClienteService.java
+++ src/main/java/com/leonardo/cursomc/services/ClienteService.java
@@ -1,11 +1,11 @@
 package com.leonardo.cursomc.services;
 
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leonardo.cursomc.domain.Cliente;
 import com.leonardo.cursomc.repositories.ClienteRepository;
+import com.leonardo.cursomc.services.exceptions.ObjectNotFoundException;
 
 import java.util.Optional;
 
