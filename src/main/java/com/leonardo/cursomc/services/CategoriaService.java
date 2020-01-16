f610cfc2e0684ec7b3083ad7dd79bc853820ed81
Leonardo Silva da Cunha <leonardo.cunha@ntendencia.com.rb>
2020-01-16 15:28:21 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/leonardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/leonardo/cursomc/services/CategoriaService.java
@@ -5,6 +5,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.leonardo.cursomc.domain.Categoria;
+
+import java.util.List;
 import java.util.Optional;
 import com.leonardo.cursomc.repositories.CategoriaRepository;
 import com.leonardo.cursomc.services.exceptions.DataIntegrityException;
@@ -40,6 +42,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 }
