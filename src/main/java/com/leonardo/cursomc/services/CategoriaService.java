424ae19d6ddc6c1d17a1c09c0b189548cbf2b0b7
Leonardo Silva da Cunha <leonardo.cunha@ntendencia.com.rb>
2020-01-17 10:57:31 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/leonardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/leonardo/cursomc/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leonardo.cursomc.domain.Categoria;
+import com.leonardo.cursomc.dto.CategoriaDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
